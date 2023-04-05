010ac9de6f5bef8f9c31a4155ebd88529303bb79
erikolvera <118490997+erikolvera@users.noreply.github.com>
2023-04-04 23:14:18 -0500

    added foundation folders and basic packages

---
+++ db/schema.sql
@@ -0,0 +1,8 @@
+-- Drop the database if it exists
+DROP DATABASE IF EXISTS library_db;
+
+-- Create the database
+CREATE DATABASE library_db;
+
+-- Switch to the employees_db database
+USE library_db;
