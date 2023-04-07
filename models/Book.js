4f1961cea1fadc91426847b3d74a272a56ae8512
erikolvera <118490997+erikolvera@users.noreply.github.com>
2023-04-06 21:43:15 -0500

    updated the models and created more tables

--- models/Book.js
+++ models/Book.js
@@ -1,30 +1,55 @@
-const {Model, DataTypes} = require('sequelize');
+const { Model, DataTypes } = require('sequelize');
 const sequelize = require('../config/connection');
 
+
+// Define Book model
 class Book extends Model {}
 
-//Created the book table
 Book.init(
-    {
-        id: {
-            type: DataTypes.INTEGER,
-            allowNull: false,
-            primaryKey: true,
-            autoIncrement: true
-        },
-        title: {
-            type: DataTypes.STRING,
-            allowNull: false
-        },
+  {
+    book_id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true
     },
-    {
-        sequelize,
-        timestamps: false,
-        freezeTableName: true,
-        underscored: true,
-        modelName: 'book'
+    title: {
+      type: DataTypes.STRING(255),
+      allowNull: false
+    },
+    total_pages: {
+      type: DataTypes.INTEGER,
+      allowNull: true
+    },
+    rating: {
+      type: DataTypes.DECIMAL(4, 2),
+      allowNull: true
+    },
+    isbn: {
+      type: DataTypes.STRING(13),
+      allowNull: true
+    },
+    published_date: {
+      type: DataTypes.DATE,
+      allowNull: true
+    },
+    publisher_id: {
+      type: DataTypes.INTEGER,
+      allowNull: true,
+      references: {
+        model: Publisher,
+        key: 'publisher_id'
+      }
     }
+  },
+  {
+    sequelize,
+    timestamps: false,
+    modelName: 'book'
+  }
 );
 
-// Exports the book model to be used in other files
+
+// Exports the models to be used in other files
 module.exports = Book;
+
+
