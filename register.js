9a57fbfec95327001e60fcb956a07168ac0752ea
Anthony Vaquer <anthonymark126@gmail.com>
2023-04-10 19:05:51 -0700

    started front-end js for sign-in

--- register.js
+++ register.js
@@ -7,21 +7,21 @@ const submitButton = document.querySelector('input[type="submit"]');
 
 submitButton.disabled = true;
 
-// validate input fields and enable/disable submit button
+//validate input fields and enable/disable submit button
 function validateForm() {
     const usernameValue = usernameInput.value.trim();
     const passwordValue = passwordInput.value.trim();
     const confirmPasswordValue = confirmPasswordInput.value.trim();
     const emailValue = emailInput.value.trim();
 
-    // check if all fields have input
+    //check if all fields have input
     if (usernameValue && passwordValue && confirmPasswordValue && emailValue) {
-      // confirm passwords match
+      //confirm passwords match
       if (passwordValue === confirmPasswordValue) {
         submitButton.disabled = false;
       } else {
         submitButton.disabled = true;
-        // show error message
+        //show error message
         confirmPasswordInput.setCustomValidity("Passwords must match");
       }
     } else {
@@ -29,14 +29,14 @@ function validateForm() {
     }
 }
 
-// event listeners for input fields
+//event listeners for input fields
 usernameInput.addEventListener('input', validateForm);
 passwordInput.addEventListener('input', validateForm);
 confirmPasswordInput.addEventListener('input', validateForm);
 emailInput.addEventListener('input', validateForm);
 
-  // event listener for form submission
-form.addEventListener('submit', (e) => {
- e.preventDefault();
+  //event listener for form submission
+form.addEventListener('submit', (a) => {
+ a.preventDefault();
     // CODE TO SUBMIT FORM TO DB GOES HERE
 });
